989453e93b37a81ad43dc9d541003d106433b732
unknown <henryenlineagt@hotmail.com>
2021-09-23 10:48:56 -0600

    adding router-dom to add roting logic with some basic access control logic based on pseudo user with a boolean simple switch

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from  "./pages/register/Register"
+import {BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const user = false; //Pseudo useer
   return (
-    <>
+    <Router>
       <TopBar />
-      {/* <Home /> */}
-      {/* <Single/> */}
-      {/* <Write/> */}
-      {/* <Settings /> */}
-      {/* <Login/> */}
-      <Register/>
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route  path="/register">{user ? <Home/> :<Register/>}<Register/></Route>
+        <Route  path="/login">{user ? <Home/> :<Login/>}</Route>
+        <Route  path="/write">{user ? <Write/> :<Register/>}</Route>
+        <Route  path="/settings">{user ? <Settings/> :<Register/>}</Route>
+        <Route  path="/single">
+          <Single/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
