950a60abd6d7ce8deadab22279315fd164aa4893
unknown <henryenlineagt@hotmail.com>
2021-09-27 15:24:48 -0600

    fixing typo on auth.js that triggerred a deprecation

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -27,7 +27,10 @@ router.post('/login', async (req, res) => {
         const user = await User.findOne({ username: req.body.username});
         !user && res.status(400).json("Wrong credentials");
 
-        const { password, ...others} = user_doc;
+        const validated = await bcrypt.compare(req.body.password, user.password);
+        !validated && res.status(400).json("Wrong credentials!");
+
+        const { password, ...others} = user._doc;
         res.status(200).json(user);
     } catch (err) {
         res.status(500).json(err);
